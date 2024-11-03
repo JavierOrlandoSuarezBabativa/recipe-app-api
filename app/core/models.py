e244ad98f1e6692bf1331139a00fa673d3e1dc04
Javier7 <javiersuarezbabativa@gmail.com>
2024-11-03 02:17:12 -0500

    The recipe and tags API's creation step

--- app/core/models.py
+++ app/core/models.py
@@ -13,7 +13,6 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     """Manager for users"""
-
     def create_user(self, email, password=None, **extra_field):
         """Create, save and return a new user"""
 
@@ -28,7 +27,6 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         """Create and return a new superuser"""
-
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -39,7 +37,6 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
-
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -49,9 +46,9 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
 
+
 class Recipe(models.Model):
     """Recipe object"""
-
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
@@ -61,6 +58,19 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    """Tag for filtering recipes"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
